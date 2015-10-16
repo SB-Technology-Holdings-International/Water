23af2e1286b27b0b8d1429e0710bb2e46fe9cb70
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-10-15 20:58:00 -0700

    Work on usage monitoring page
    responsive graphs!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,27 +34,27 @@ function loadJSON(path, success, error)
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
-  app.data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+  app.usageTimeData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [
           {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            label: 'My First dataset',
+            fillColor: 'rgba(220,220,220,0.2)',
+            strokeColor: 'rgba(220,220,220,1)',
+            pointColor: 'rgba(220,220,220,1)',
+            pointStrokeColor: '#fff',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(220,220,220,1)',
             data: [65, 59, 80, 81, 56, 55, 40]
           },
           {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
+            label: 'My Second dataset',
+            fillColor: 'rgba(151,187,205,0.2)',
+            strokeColor: 'rgba(151,187,205,1)',
+            pointColor: 'rgba(151,187,205,1)',
+            pointStrokeColor: '#fff',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(151,187,205,1)',
             data: [28, 48, 40, 19, 86, 27, 90]
           }
         ]
@@ -126,6 +126,13 @@ function loadJSON(path, success, error)
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
+  });
+
+  addEventListener('iron-select', function () {
+      if (app.route === 'usage') {
+        app.loadCharts = true;
+
+      }
   });
 
 
