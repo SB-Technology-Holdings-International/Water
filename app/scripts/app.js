05d063cee37ee6b0b71f770e87b5f648c4d29b37
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-10-15 21:54:49 -0700

    More work on usage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,30 +35,57 @@ function loadJSON(path, success, error)
   var app = document.querySelector('#app');
 
   app.usageTimeData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'My First dataset',
-            fillColor: 'rgba(220,220,220,0.2)',
-            strokeColor: 'rgba(220,220,220,1)',
-            pointColor: 'rgba(220,220,220,1)',
-            pointStrokeColor: '#fff',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(220,220,220,1)',
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-          {
-            label: 'My Second dataset',
-            fillColor: 'rgba(151,187,205,0.2)',
-            strokeColor: 'rgba(151,187,205,1)',
-            pointColor: 'rgba(151,187,205,1)',
-            pointStrokeColor: '#fff',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(151,187,205,1)',
-            data: [28, 48, 40, 19, 86, 27, 90]
-          }
-        ]
-      };
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: 'My Second dataset',
+        fillColor: 'rgba(151,187,205,0.2)',
+        strokeColor: 'rgba(151,187,205,1)',
+        pointColor: 'rgba(151,187,205,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(151,187,205,1)',
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }
+    ]
+  };
+
+  app.usageRelativeData = [
+        {
+          value: 300,
+          color:'#F7464A',
+          highlight: '#FF5A5E',
+          label: 'Valve 1'
+        },
+        {
+          value: 50,
+          color: '#46BFBD',
+          highlight: '#5AD3D1',
+          label: 'Valve 2'
+        },
+        {
+          value: 400,
+          color: '#ADB45C',
+          highlight: '#AFC870',
+          label: 'Valve 3'
+        },
+        {
+          value: 100,
+          color: '#FDB45C',
+          highlight: '#FFC870',
+          label: 'Valve 4'
+        }
+      ];
 
   app.newPlants = [];
 
