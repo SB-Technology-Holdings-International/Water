e85a19b8380c5985cde428435bc36ae5dc6df1f2
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-11-06 11:10:51 -0800

    Change images

--- gen-images.sh
+++ gen-images.sh
@@ -1,6 +1,7 @@
 cd ~/Code/Water
 rm app/images/touch/*
 cp -f logo.svg app/images/logo.svg
+convert -background transparent -resize 192x192 logo.svg app/images/touch/chrome-splashscreen-icon-384x384.png
 convert logo-square.svg -resize 152x152 app/images/touch/apple-touch-icon.png
 convert -background transparent -resize 192x192 logo.svg app/images/touch/chrome-touch-icon-192x192.png
 convert -background transparent -resize 128x128 logo.svg app/images/touch/icon-128x128.png
