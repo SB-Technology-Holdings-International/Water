3a9206964206e7f8ed7ee2de3d756bcc2b2d9e9a
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-12-02 16:34:53 -0800

    Work on device code

--- device/main.py
+++ device/main.py
@@ -5,87 +5,62 @@ import SocketServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import threading
 
-class Server(threading.Thread):
-    def __init__(self):
-        super(Server, self).__init__()
-        self._stop = threading.Event()
-    def run(self):
 
-        class RequestHandler(BaseHTTPRequestHandler):
-            def do_GET(self):
-                if self.path == '/local-control':
-                    # Insert code here
-                    pass
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/local-control':
+            # Insert code here
+            pass
 
-                self.send_response(200)
+        self.send_response(200)
 
-        httpd = SocketServer.TCPServer(("", 8080), RequestHandler)
-        httpd.serve_forever()
-    def stop(self):
-        self._stop.set()
-    def stopped(self):
-        return self._stop.isSet()
+httpd = SocketServer.TCPServer(("", 8080), RequestHandler)
 
-class Controller(threading.Thread):
-    def __init__(self):
-        super(Controller, self).__init__()
-        self._stop = threading.Event()
-    def run(self):
-        # Valve GPIO pin mapping
-        valves = [135, 136, 137, 138] # Not actual values
-        # Setup GPIO
-        #gpio.pin_mode(valves[0], 'out')
-        #gpio.pin_mode(valves[1], 'out')
-        #gpio.pin_mode(valves[2], 'out')
-        #gpio.pin_mode(valves[3], 'out')
-        # Define variables
-        last_update = ''
-        schedule = [(0, 1000), (0, 1009)] # (valve number, seconds past midnight)
+def server(a, stop_event):
 
-        while True:
-            # Update once a day
-            if last_update != datetime.date.today():
-                print "Updating from server..."
-                # Fetch latest data
-                # If things work out:
-                last_update = datetime.date.today()
-
-            if schedule != []:
-                if datetime.datetime.now() > (datetime.datetime.combine(datetime.date.today(),
-                                              datetime.datetime.min.time()) +
-                                              datetime.timedelta(seconds=schedule[0][1])):
-                    print "Turning on"
-                    #gpio.digital_write(valves[schedule[0][0]], 1)
-                    schedule.pop(0)
-
-            if self._stop.isSet():
-                print "Controller off"
-                break
-            # Reduce cpu usage
-            time.sleep(0.1)
-    def stop(self):
-        self._stop.set()
-    def stopped(self):
-        return self._stop.isSet()
+    httpd.serve_forever()
 
 
-def main():
-    t1_stop= threading.Event()
-    t1 = threading.Thread(target=thread1, args=(1, t1_stop))
+def controller(a, stop_event):
+    # Valve GPIO pin mapping
+    valves = [135, 136, 137, 138] # Not actual values
+    # Setup GPIO
+    #gpio.pin_mode(valves[0], 'out')
+    #gpio.pin_mode(valves[1], 'out')
+    #gpio.pin_mode(valves[2], 'out')
+    #gpio.pin_mode(valves[3], 'out')
+    # Define variables
+    last_update = ''
+    schedule = [(0, 1000), (0, 1009)] # (valve number, seconds past midnight)
+    print "ok"
+    while(not stop_event.isSet()):
+        # Update once a day
+        if last_update != datetime.date.today():
+            print "Updating from server..."
+            # Fetch latest data
+            # If things work out:
+            last_update = datetime.date.today()
 
-    t2_stop = threading.Event()
-    t2 = threading.Thread(target=thread2,  args=(2, t2_stop))
+        if schedule != []:
+            if datetime.datetime.now() > (datetime.datetime.combine(datetime.date.today(),
+                                          datetime.datetime.min.time()) +
+                                          datetime.timedelta(seconds=schedule[0][1])):
+                print "Turning on"
+                #gpio.digital_write(valves[schedule[0][0]], 1)
+                schedule.pop(0)
 
-    raw_input("Press enter to stop")
-    print "Stopping"
-    #stop the thread2
-    t1_stop.set()
-    t2_stop.set()
+        # Reduce cpu usage
+        time.sleep(0.5)
+        print "Test"
 
-Server().start()
-a = Controller().start()
-print a
-raw_input("Press enter to stop ")
-print "Stopping"
-a.stop()
-Server().stop()
+t1_stop= threading.Event()
+t1 = threading.Thread(target=server, args=(1, t1_stop))
+t1.start()
+
+t2_stop = threading.Event()
+t2 = threading.Thread(target=controller, args=(1, t2_stop))
+t2.start()
+raw_input("Press enter to stop")
+t2_stop.set()
+t1_stop.set()
+httpd.server_close()
