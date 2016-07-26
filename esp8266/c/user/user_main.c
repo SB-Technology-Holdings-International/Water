db4c75feab7f2e1098e9d653c544f25cf1dfefb7
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2016-07-25 20:31:07 -0700

    Attempt using Arduino instead of c

--- esp8266/c/user/user_main.c
+++ esp8266/c/user/user_main.c
@@ -7,6 +7,29 @@
 
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
+
+//Allocate an "espconn"
+pHTTPServer = (struct espconn *)os_zalloc(sizeof(struct espconn));
+ets_memset( pHTTPServer, 0, sizeof( struct espconn ) );
+
+//Initialize the ESPConn
+espconn_create( pHTTPServer );
+pHTTPServer->type = ESPCONN_TCP;
+pHTTPServer->state = ESPCONN_NONE;
+
+//Make it a TCP conention.
+pHTTPServer->proto.tcp = (esp_tcp *)os_zalloc(sizeof(esp_tcp));
+pHTTPServer->proto.tcp->local_port = 80;
+
+//"httpserver_connectcb" gets called whenever you get an incoming connetion.
+espconn_regist_connectcb(pHTTPServer, server_connectcb);
+
+//Start listening!
+espconn_accept(pHTTPServer);
+
+//I don't know what default is, but I always set this.
+espconn_regist_time(pHTTPServer, 15, 0);
+
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void loop(os_event_t *events);
 
